68c43f30ec9faa90ef3bdff95486e3a86c267b2e
Karla Agraz <karla.agraz@gmail.com>
2020-03-20 00:46:11 -0500

    configuración

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  theme: {
-    extend: {},
+  theme: { 
+    fontFamily: {
+    display: ['Gilroy', 'sans-serif'],
+    body: ['Graphik', 'sans-serif'],
+  },
+  extend: {
+    colors: {
+      cyan: '#9cdbff',
+    },
   },
   variants: {},
   plugins: [],
