362ee8e14c2e5b605001e97a6e7688df40b5ff2b
Karla Agraz <karla.agraz@gmail.com>
2020-03-20 01:01:58 -0500

    primeros pasos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   theme: { 
-    fontFamily: {
-    display: ['Gilroy', 'sans-serif'],
-    body: ['Graphik', 'sans-serif'],
-  },
-  extend: {
-    colors: {
-      cyan: '#9cdbff',
-    },
+  extend: {},
   },
   variants: {},
   plugins: [],
